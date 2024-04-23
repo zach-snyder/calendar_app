8fa8da3d271b40c2c7498d2062f88bc31a781f52
zach-snyder <zach@Zacharys-Laptop>
2024-04-22 18:10:30 -0600

    cheap_working

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,6 +133,3 @@ MEDIA_URL = '/images/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/login/'
